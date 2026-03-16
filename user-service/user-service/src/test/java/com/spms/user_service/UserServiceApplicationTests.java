7cd5635224535975c59187cc163b2875e359f998
ASUS <nethmidileksha6@gmail.com>
2026-03-16 07:51:09 +0530

    initial project structure

---
+++ user-service/user-service/src/test/java/com/spms/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spms.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
